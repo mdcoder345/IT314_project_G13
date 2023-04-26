f9f879b2728fd401cbb3056cb24b184e479cd388
kavan02 <kavanbuch02@gmail.com>
2023-04-26 20:48:10 +0530

    Completed REST Api testing for courses collection and users collection

--- Development/backend/test/course.test.js
+++ Development/backend/test/course.test.js
@@ -6,12 +6,52 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Course", () => {
-  //add course module
-  describe("POST /courses", () => {
-    it("it should add course", (done) => {
+  let cookie;
+  //login module
+  describe("POST /login", () => {
+    it("it should login user", (done) => {
+      chai
+        .request(app)
+        .post("/login")
+        .send({
+          username_email: "kavan",
+          password: "kavan",
+        })
+        .end((err, res) => {
+          res.statusCode.should.equal(200);
+          res.body.should.be.a("object");
+          cookie = res.headers["set-cookie"][0];
+          done();
+        });
+    });
+  });
+
+  //read course module
+  describe("GET /courses", () => {
+    it("it should read courses", (done) => {
       chai
         .request(app)
         .get("/courses")
+        .set("Cookie", cookie)
+        .end((err, res) => {
+          res.statusCode.should.equal(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  //add course module
+  describe("POST /courses", () => {
+    it("it should add courses", (done) => {
+      chai
+        .request(app)
+        .post("/courses")
+        .set("Cookie", cookie)
+        .send({
+          courseName: "DSA",
+          courseDescription: "Data Structures and Algorithms",
+        })
         .end((err, res) => {
           res.statusCode.should.equal(200);
           res.body.should.be.a("object");
@@ -26,9 +66,10 @@ describe("Course", () => {
       chai
         .request(app)
         .patch("/courses/:id")
+        .set("Cookie", cookie)
         .send({
-          courseName: "DSA",
-          courseDescription: "Data Structures and Algorithms",
+          courseName: "Dev-ops",
+          courseDescription: "Development Operations infinite cycle",
         })
         .end((err, res) => {
           res.statusCode.should.equal(200);
@@ -44,6 +85,7 @@ describe("Course", () => {
       chai
         .request(app)
         .delete("/courses/:id")
+        .set("Cookie", cookie)
         .end((err, res) => {
           res.statusCode.should.equal(200);
           res.body.should.be.a("object");
